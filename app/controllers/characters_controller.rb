0ccd9e8f109ea502db2500aab3192c4c534893e4
Sean Durham <sean@sdurham.net>
2013-07-10 17:39:52 -0700

    Characters controller rejects malformed characters. Sets character class to 'Wizard' by default.

--- app/controllers/characters_controller.rb
+++ app/controllers/characters_controller.rb
@@ -7,17 +7,21 @@ class CharactersController < ApplicationController
     @character = Character.new
   end
 
-  def create
-    @character = Character.new character_params
 
+  def create
+    @character = Character.new(character_params)
+    @character.character_class = CharacterClass.find_by_name('Wizard')
     @character.save!
     redirect_to characters_path
+  rescue
+    flash[:error] = 'The character could not be created'
+    render :new
   end
 
   private
 
   def character_params
-    params.require(:character).permit(:name)
+    params.require(:character).permit(:name, :character_class_id)
   end
 
 end
--- spec/controllers/characters_controller_spec.rb
+++ spec/controllers/characters_controller_spec.rb
@@ -2,10 +2,12 @@ require 'spec_helper'
 
 describe CharactersController do
   before do
-    @char = FactoryGirl.create :character
+    FactoryGirl.create :character_class, name: 'Wizard'
   end
 
   describe 'GET index' do
+    let(:character) { FactoryGirl.create :character }
+
     it 'is successful' do
       get :index
       expect(response).to be_success
@@ -13,7 +15,7 @@ describe CharactersController do
 
     it 'assigns characters' do
       get :index
-      expect(assigns :characters).to include @char
+      expect(assigns :characters).to include character
     end
   end
 
@@ -26,11 +28,33 @@ describe CharactersController do
 
   describe 'POST create' do
     describe 'with valid params' do
+      let(:character_attrs) { FactoryGirl.build(:character).attributes }
+
       it 'creates the character' do
         expect do
-          post :create, character: { name: 'Bout' }
+          post :create, character: character_attrs
         end.to change { Character.count }.by 1
         expect(subject).to redirect_to characters_path
+      end
+
+      it 'sets the character_class to Wizard' do
+        post :create, character: character_attrs.except('character_class_id')
+        character = Character.last
+        expect(character.character_class.name).to eq 'Wizard'
+      end
+    end
+
+    describe 'with invalid params' do
+      let(:character_attrs) { FactoryGirl.build(:character).attributes.except('name') }
+
+      it 'rerenders the new page' do
+        post :create, character: character_attrs
+        expect(response).to render_template(:new)
+      end
+
+      it 'informs the user of the error' do
+        post :create, character: character_attrs
+        expect(flash[:error]).not_to be_nil
       end
     end
   end
