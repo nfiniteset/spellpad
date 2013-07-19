d3ea6243db20cb6377496101095ffb496056a50b
Sean Durham <sean@sdurham.net>
2013-07-18 23:25:15 -0500

    Clean up spells#index page.

--- spec/controllers/spells_controller_spec.rb
+++ spec/controllers/spells_controller_spec.rb
@@ -1,23 +1,13 @@
 require 'spec_helper'
 
-def assigned_spells
-  assigns[:spell_levels].values.flatten.map(&:spell)
-end
-
 describe SpellsController do
   describe '#GET index' do
     describe 'with a character_class_id' do
       let(:wizard_class) { FactoryGirl.create :character_class, name: 'Wizard' }
-      let(:wizard_spell) { FactoryGirl.create :spell, name: 'Sparkle Pony' }
-      let!(:wizard_spell_level) { FactoryGirl.create :spell_level, spell_class_id: wizard_class.id, spell_class_type: wizard_class.class.name, spell: wizard_spell, level: 1 }
-      let(:bard_class) { FactoryGirl.create :character_class, name: 'Bard' }
-      let(:bard_spell) { FactoryGirl.create :spell, name: 'The Bear and the Maiden Fare' }
-      let!(:bard_spell_level) { FactoryGirl.create :spell_level, spell_class_id: bard_class.id, spell_class_type: bard_class.class.name, spell: bard_spell, level: 1 }
 
-      it 'assigns spells belonging to that character class' do
+      it 'assigns that character class' do
         get :index, character_class_id: wizard_class.id
-        expect(assigned_spells).to include wizard_spell
-        expect(assigned_spells).to_not include bard_spell
+        expect(assigns[:spell_class]).to eq wizard_class
       end
     end
 
