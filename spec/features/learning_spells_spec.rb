5e8f798285f47c9cb6238f220da33729b4b9dcb7
Sean Durham <sean@sdurham.net>
2013-07-24 22:41:37 -0700

    WIP

--- spec/features/learning_spells_spec.rb
+++ spec/features/learning_spells_spec.rb
@@ -1,24 +1,26 @@
 require 'spec_helper'
 
 feature "Learning spells" do
-
+  given(:user) { FactoryGirl.create :user }
   given(:wizard_class) { FactoryGirl.create :character_class, name: 'Wizard' }
   given(:wizard_spell) { FactoryGirl.create :spell, name: 'Arglebargle'}
   given!(:wizard_spell_level) do
-    FactoryGirl.create :spell_level, spell_class_id: wizard_class.id, 
-                                     spell_class_type: wizard_class.class.name, 
-                                     spell: wizard_spell, 
+    FactoryGirl.create :spell_level, spell_class_id: wizard_class.id,
+                                     spell_class_type: wizard_class.class.name,
+                                     spell: wizard_spell,
                                      level: 3
   end
   given!(:character) do
-    FactoryGirl.create :character, name: 'Sophie', 
-                                   character_class: wizard_class, 
-                                   known_spells: [wizard_spell]
+    FactoryGirl.create :character, name: 'Sophie',
+                                   character_class: wizard_class,
+                                   known_spells: [wizard_spell],
+                                   user: user
   end
 
   scenario "marks spells known by a character" do
-    visit root_path
-    click_on 'Sophie'
+    pending
+    sign_in user
+    select_character 'Sophie'
     click_on 'All Spells'
     expect(find_link(wizard_spell.name)).to have_css('.known')
   end
